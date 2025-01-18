cc9f5199a4b57d8908315bf313864b26c37d4ae1
MILANA LABAZANOVA <milanamsu@gmail.com>
2025-01-18 23:30:22 +0000

    appleVersion2

--- components/buttons/BeginScanButton.tsx
+++ components/buttons/BeginScanButton.tsx
@@ -369,7 +369,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   textContainer: {
-    // Original "Start" / "Scan" icons layout
+    // Original "Start" / "Scan" icons 
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
